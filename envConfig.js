12cff1b3307d9c51a39b79d1de35d1b3cfcbc528
tongzhuomasi <youyadexuanlv@qq.com>
2021-09-24 18:15:13 +0800

    bug fix

--- envConfig.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable import/prefer-default-export */
-const env = process.env.NODE_ENV;
-
-console.log(env);
-const BASE_URL = env === 'development'
-  ? 'http://localhost:3000'
-  : 'http://localhost:3000';
-
-export { BASE_URL };
