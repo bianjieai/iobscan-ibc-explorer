feb1e86e303d270c4ef3990e694312e454dfbfbd
jinbiao <zjb742626083>
2022-01-13 19:03:07 +0800

    change vue config

--- vue.config.js
+++ vue.config.js
@@ -14,17 +14,12 @@ module.exports = {
     },
   },
   devServer: {
-    proxy: {
-      '/api': {
-          // target: 'http://192.168.150.60:38080/api/',
-          // target: 'https://stage.ibc.iobscan.io/api/',
-          // target: 'http://10.1.4.118:3000/',
-          // target: 'https://ibc.iobscan.io/api',
-          // target: 'http://10.1.4.118:3000/',
-          target: 'http://192.168.150.60:38080/api/',
-        secure: false,
-        pathRewrite: { '^/api': '' },
+      proxy: {
+          '/api': {
+              target: 'http://localhost:3000',
+              secure: false,
+              pathRewrite: { '^/api': '' },
+          },
       },
-    },
   },
 };
