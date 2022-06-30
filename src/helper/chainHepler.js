3740356ba78fafc03840bc1d02cf374d4d4c367b
hangTaishan <hangTaishan@bishuilantiandeMacBook-Pro.local>
2022-06-30 12:11:27 +0800

    #

--- src/helper/chainHepler.js
+++ src/helper/chainHepler.js
@@ -1,6 +1,5 @@
 export default class ChainHelper {
     static formatChainId(chainId){
-        console.log('=======:',chainId);
         if (chainId && chainId !== '--') {
             return chainId.replace(new RegExp("\_", "g"), "-")
         }
