dafca608b2ec1a157d7f734c4fc67adaff253c63
shanshan <shan@bianjie.ai>
2022-07-06 19:11:34 +0800

    sort chains

--- src/helper/chainHepler.js
+++ src/helper/chainHepler.js
@@ -12,25 +12,36 @@ export default class ChainHelper {
     
     // chain_name sort
     static sortByChainName(sourceList) {
+        function changeChainsSort(matchChainA, matchChainB, item) {
+            /**
+             * -1 -- need not
+             *  0 -- same
+             *  1 -- need
+             */
+            let saveChain, saveChannel;
+            if(matchChainA?.chain_name.localeCompare(matchChainB?.chain_name) === 1) {
+                saveChain = item.chain_a;
+                item.chain_a = item.chain_b;
+                item.chain_b = saveChain;
+                saveChannel = item.channel_a;
+                item.channel_a = item.channel_b;
+                item.channel_b = saveChannel;
+            }
+        }
         if(isArray(sourceList) && sourceList?.length) {
             const updateList = sourceList?.map((item, index) => {
                 const matchChainA = allChains?.value?.all?.find((chain) => chain.chain_id === item.chain_a);
                 const matchChainB = allChains?.value?.all?.find((chain) => chain.chain_id === item.chain_b);
-                /**
-                 * -1 -- need not
-                 *  0 -- same
-                 *  1 -- need
-                 */
-                if(matchChainA?.chain_name !== 'Cosmos Hub' && (matchChainA?.chain_name !== 'IRIS Hub' || (matchChainA?.chain_name === 'IRIS Hub' && matchChainB?.chain_name === 'Cosmos Hub'))) {
-                    let saveChain, saveChannel;
-                    if(matchChainA?.chain_name.localeCompare(matchChainB?.chain_name) === 1) {
-                        saveChain = item.chain_a;
-                        item.chain_a = item.chain_b;
-                        item.chain_b = saveChain;
-                        saveChannel = item.channel_a;
-                        item.channel_a = item.channel_b;
-                        item.channel_b = saveChannel;
+                if([matchChainA?.chain_name, matchChainB?.chain_name].indexOf('Cosmos Hub') !== -1) {
+                    if([matchChainA?.chain_name, matchChainB?.chain_name].indexOf('Cosmos Hub') === 1) {
+                        changeChainsSort(matchChainA, matchChainB, item);
                     }
+                } else if([matchChainA?.chain_name, matchChainB?.chain_name].indexOf('IRIS Hub') !== -1) {
+                    if([matchChainA?.chain_name, matchChainB?.chain_name].indexOf('IRIS Hub') === 1) {
+                        changeChainsSort(matchChainA, matchChainB, item);
+                    }
+                } else {
+                    changeChainsSort(matchChainA, matchChainB, item);
                 }
                 return item;
             })
