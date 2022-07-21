42fb3e013ef3738d9824cda78669fd890cf4ee3a
shanshan <shan@bianjie.ai>
2022-07-21 16:46:07 +0800

    update

--- src/composables/index.ts
+++ src/composables/index.ts
@@ -17,15 +17,13 @@ export const useTimeInterval = (intervalCallBack: Function, interval = ageTimerI
 }
 
 export const useChangeTitleAndIcon = () => {
-    onMounted(() => {
-        if(import.meta.env.MODE === 'staging') {
-            let link: any = document.querySelector("link[rel *= 'icon']") || document.createElement("link");
-            let title: any = document.querySelector("title") || document.createElement("title");
-            link.rel = "icon";
-            link.href = import.meta.env.VITE_FAVICON;
-            title.innerHTML = import.meta.env.VITE_TITLE;
-            document.getElementsByTagName("head")[0].appendChild(link);
-            document.getElementsByTagName("head")[0].appendChild(title);
-        }
-    })
+    if(import.meta.env.MODE === 'development') {
+        let link: any = document.querySelector("link[rel *= 'icon']") || document.createElement("link");
+        let title: any = document.querySelector("title") || document.createElement("title");
+        link.rel = "icon";
+        link.href = import.meta.env.VITE_FAVICON;
+        title.innerHTML = import.meta.env.VITE_TITLE;
+        document.getElementsByTagName("head")[0].appendChild(link);
+        document.getElementsByTagName("head")[0].appendChild(title);
+    }
 }
