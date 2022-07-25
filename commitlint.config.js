d2f6e44fc140baad059a0142959eab771e3dcc28
duanjie-cmd <18870420982@139.com>
2022-07-25 22:16:33 +0800

    adjust init

--- commitlint.config.js
+++ commitlint.config.js
@@ -19,14 +19,14 @@ module.exports = {
         ],
         // <type> 格式 小写
         'type-case': [2, 'always', 'lower-case'],
-        // <type> 不能为空
-        'type-empty': [2, 'never'],
-        // <scope> 范围不能为空
-        'scope-empty': [2, 'never'],
+        // <type> 可以为空
+        'type-empty': [0, 'never'],
+        // <scope> 可以为空
+        'scope-empty': [0, 'never'],
         // <scope> 范围格式
         'scope-case': [0],
         // <subject> 主要 message 不能为空
-        'subject-empty': [2, 'never'],
+        'subject-empty': [0, 'never'],
         // <subject> 以什么为结束标志，禁用
         'subject-full-stop': [0, 'never'],
         // <subject> 格式，禁用
