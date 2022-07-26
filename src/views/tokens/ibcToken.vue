d295651ba76d3898d8937d7136939f3fa224e899
shanshan <shan@bianjie.ai>
2022-07-26 10:43:44 +0800

    update footer icon

--- src/views/tokens/ibcToken.vue
+++ src/views/tokens/ibcToken.vue
@@ -109,10 +109,9 @@ const baseDenomInfo = computed(() => {
       symbol = filterSymbol
     }
   }
-
   return {
     symbol,
-    imgSrc: filterData[0]?.icon ?? new URL('../../assets/token-default.png', import.meta.url).href
+    imgSrc: filterData[0]?.icon ? filterData[0]?.icon : new URL('../../assets/token-default.png', import.meta.url).href
   }
 })
 
