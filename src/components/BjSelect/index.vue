7e9d45471e7b3913f308bb6dc5e529ee25711b4c
duanjie-cmd <18870420982@139.com>
2022-08-08 17:52:05 +0800

    update iconfont

--- src/components/BjSelect/index.vue
+++ src/components/BjSelect/index.vue
@@ -32,7 +32,7 @@
             </template>
             <!--            多选单选的展示 end-->
             <span class="button_icon flex justify-between items-center">
-                <svg
+                <!-- <svg
                     :style="{ transform: visible ? 'rotate(180deg)' : 'rotate(0)' }"
                     focusable="false"
                     data-icon="down"
@@ -46,7 +46,14 @@
                     <path
                         d="M884 256h-75c-5.1 0-9.9 2.5-12.9 6.6L512 654.2 227.9 262.6c-3-4.1-7.8-6.6-12.9-6.6h-75c-6.5 0-10.3 7.4-6.5 12.7l352.6 486.1c12.8 17.6 39 17.6 51.7 0l352.6-486.1c3.9-5.3.1-12.7-6.4-12.7z"
                     ></path>
-                </svg>
+                </svg> -->
+                <i
+                    :class="[visible ? 'visible_color' : '']"
+                    class="iconfont icon-zhankai-copy-icon"
+                    :style="{
+                        transform: visible ? 'rotate(180deg)' : 'rotate(0)'
+                    }"
+                ></i>
             </span>
         </div>
 
@@ -344,11 +351,12 @@
 
     .button_icon {
         transition: all 0.3s cubic-bezier(0.645, 0.045, 0.355, 1);
-        padding: 0 6px;
+        padding: 0 2px;
         border-left: 1px solid var(--bj-border-color);
         height: 36px;
 
-        & > svg {
+        .iconfont {
+            font-size: 18px;
             color: var(--bj-text-third);
         }
     }
