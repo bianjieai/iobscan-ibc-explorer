b0cc81c9cfab97486f98538fb4fffea80efb2902
wuxiang <wuxiang@bianjie.ai>
2022-08-11 20:12:12 +0800

    :fix bug 822

--- src/constants/bjSelect.ts
+++ src/constants/bjSelect.ts
@@ -1,2 +1,3 @@
 export const Token_ICON = new URL('../assets/token-default.png', import.meta.url).href;
 export const CHAIN_ICON = new URL('../assets/home/chain-default.png', import.meta.url).href;
+export const TIP_ICON = new URL('../assets/tip.png', import.meta.url).href;
