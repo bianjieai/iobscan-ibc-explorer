8e9bda62ef2db64a5568972f3c4baf230d37ef32
shanshan <shan@bianjie.ai>
2022-09-09 11:50:42 +0800

    chain_info a half

---
+++ src/components/expandbtn/ExpandBtn.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="expand" @click="expandInfo">
+        <span class="expand__btn">Click to see More</span>
+        <i
+            class="expand__icon iconfont icon-shouqi"
+            :class="isShowDetailsInfo ? '' : 'expand__more'"
+        >
+        </i>
+    </div>
+</template>
+
+<script setup lang="ts">
+    interface IProps {
+        isShowDetailsInfo: boolean;
+    }
+    const props = defineProps<IProps>();
+    const emits = defineEmits<{
+        (e: 'updateIsShowDetailsInfo', newIsShowDetailsInfo: boolean): void;
+    }>();
+    const expandInfo = () => {
+        emits('updateIsShowDetailsInfo', !props.isShowDetailsInfo);
+    };
+</script>
+
+<style lang="less" scoped>
+    .expand {
+        margin-top: 16px;
+        font-size: var(--bj-font-size-normal);
+        font-weight: 400;
+        color: var(--bj-primary-color);
+        line-height: 14px;
+        &__btn {
+        }
+        &__icon {
+            display: inline-block;
+            margin-left: 6px;
+            font-size: 8px;
+        }
+        &__more {
+            transform: rotate(180deg);
+        }
+    }
+</style>
---
+++ src/views/transfers/details/components/ChainBaseInfo.vue
@@ -0,0 +1,64 @@
+<template>
+    <div class="chain_info">
+        <TitleCard title="From"></TitleCard>
+        <div class="chain_info__details">
+            <div class="chain_info__address">
+                <span class="chain_info__label">{{ chainAddress.label }}</span>
+                <span class="chain_info__value">{{ chainAddress.value }}</span>
+            </div>
+            <div class="chain_info__chain_id">
+                <span class="chain_info__label">{{ chainInfoList.label }}</span>
+                <span class="chain_info__value">
+                    <span class="chain_info__icon">
+                        <img src="" alt="" />
+                    </span>
+                    <span>{{ chainInfoList.value }}</span>
+                </span>
+            </div>
+            <div class="chain_info__list">
+                <div
+                    v-for="(item, index) in chainInfoListExpand"
+                    :key="index"
+                    class="chain_info__list_expand__item"
+                >
+                    <span class="chain_info__label">{{ item.label }}</span>
+                    <span class="chain_info__value">{{ item.value }}</span>
+                </div>
+            </div>
+            <div v-if="isShowChainDetailsInfo" class="chain_info__list_expand">
+                <div
+                    v-for="(item, index) in chainInfoListExpand"
+                    :key="index"
+                    class="chain_info__list_expand__item"
+                >
+                    <span class="chain_info__label">{{ item.label }}</span>
+                    <span class="chain_info__value">{{ item.value }}</span>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+    import { ITxInfo } from '@/types/interface/transfers.interface';
+    import { useChainInfo } from '@/views/transfers/details/composable';
+    import TitleCard from './TitleCard.vue';
+    interface IProps {
+        chainInfo: ITxInfo | undefined;
+    }
+    const props = defineProps<IProps>();
+
+    const { chainAddress, chainInfoList, chainInfoListExpand, isShowChainDetailsInfo } =
+        useChainInfo(props);
+</script>
+
+<style lang="less" scoped>
+    .chain_info {
+        &__details {
+            margin-top: 16px;
+            padding: 16px;
+            background: #f8fafd;
+            border-radius: var(--border-radius-normal);
+        }
+    }
+</style>
