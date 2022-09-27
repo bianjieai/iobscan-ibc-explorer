41037dfed1f496e624fb9871147b009b0838854d
duanjie-cmd <18870420982@139.com>
2022-09-27 15:30:20 +0800

    update gtag

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,8 @@ import './theme/index.less';
 import './assets/icon/iconfont';
 import './assets/icon/iconfont.css';
 import { createPinia } from 'pinia';
-// todo replace
-// import { injectGtag } from '@/utils/injectGtag';
-// injectGtag();
+import { injectGtag } from '@/utils/injectGtag';
+injectGtag();
 
 const app = createApp(App);
 app.use(router).use(createPinia()).mount('#app');
