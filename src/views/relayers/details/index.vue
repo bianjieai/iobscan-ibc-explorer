4d67bbf50d834102e1a5efd65b9a8bd077a6e341
duanjie-cmd <18870420982@139.com>
2022-11-09 17:25:00 +0800

    update bar demo

--- src/views/relayers/details/index.vue
+++ src/views/relayers/details/index.vue
@@ -15,7 +15,18 @@
         </InfoCard>
         <div class="relayer_details__charts_wrap">
             <div class="relayer_details__charts_wrap__left">
-                <div class="relayer_details__charts_wrap__left__top"></div>
+                <div class="relayer_details__charts_wrap__left__top">
+                    <InfoCard
+                        class="relayer_details__charts_wrap__left__top__relayed_trend_wrap"
+                        icon="icon-Trend"
+                        title="Last 30D Relayed Trend"
+                        :is-show-choose-btn="true"
+                        :default-choose-btn="relayedTrendChoose"
+                        @change-choose-btn="relayedTrendChooseBtnFn"
+                    >
+                        <div ref="relayedTrendDom" style="width: 756px; height: 324px"></div>
+                    </InfoCard>
+                </div>
                 <div class="relayer_details__charts_wrap__left__bottom">
                     <InfoCard
                         class="relayer_details__charts_wrap__left__bottom__transfer_type_wrap"
@@ -54,7 +65,16 @@
                     </InfoCard>
                 </div>
             </div>
-            <div class="relayer_details__charts_wrap__right"></div>
+            <div class="relayer_details__charts_wrap__right">
+                <InfoCard
+                    class="relayer_details__charts_wrap__right__relayed_trend_wrap"
+                    icon="icon-a-relayedassets"
+                    title="Related Assets"
+                    :is-show-choose-btn="true"
+                    :default-choose-btn="0"
+                >
+                </InfoCard>
+            </div>
         </div>
     </PageContainer>
 </template>
@@ -69,6 +89,7 @@
         useGetSuccessRatePercent,
         useGetTransferTypeData
     } from './composable';
+    import * as echarts from 'echarts';
     const {
         relayerIcon,
         relayerName,
@@ -88,6 +109,238 @@
         timeoutPacketTxsPercent
     } = useGetTransferTypeData();
     const { successRatePercent } = useGetSuccessRatePercent(relayedSuccessTxs, relayedTotalTxs);
+
+    // todo dj 逻辑抽离
+    const relayedTrendChoose = ref(0);
+    const relayedTrendDom = ref<HTMLElement>();
+    const relayedTrendChooseBtnFn = (index: number) => {
+        relayedTrendChoose.value = index;
+    };
+    onMounted(() => {
+        const relayedTrendChart = echarts.init(relayedTrendDom.value as HTMLElement);
+        let option = {
+            grid: {
+                left: 24,
+                right: 24,
+                top: 32,
+                bottom: 24,
+                containLabel: true
+            },
+            tooltip: {
+                trigger: 'item',
+                position: 'top',
+                backgroundColor: null,
+                borderWidth: 0,
+                padding: 0,
+                extraCssText: 'box-shadow: 0 0 0 transparent;',
+                formatter: (params: any) => {
+                    return `<div style="display: flex; flex-direction: column; align-items: center; transform: translate(0,6px);">
+                                  <div
+                                      style="
+                                          display: flex;
+                                          flex-direction: column;
+                                          margin-bottom: 4px;
+                                          background: #ffffff;
+                                          box-shadow: 0px 2px 8px 0px #d9deec;
+                                          border-radius: 4px;
+                                          border: 1px solid #d9dfee;
+                                      "
+                                  >
+                                      <div style="display: flex; justify-content: flex-start; padding: 12px 12px 8px">
+                                          <span
+                                              style="
+                                                  font-size: 14px;
+                                                  font-family: 'GolosUI_Medium';
+                                                  color: #000;
+                                                  font-weight: 500;
+                                                  line-height: 18px;
+                                              "
+                                              >Transfers:
+                                          </span>
+                                          <span
+                                              style="
+                                                  margin-left: 8px;
+                                                  font-size: 14px;
+                                                  color: rgba(0, 0, 0, 0.75);
+                                                  font-family: 'GolosUIWebRegular';
+                                                  font-weight: 400;
+                                                  line-height: 18px;
+                                              "
+                                              >${params.data}</span
+                                          >
+                                      </div>
+                                      <div style="display: flex; justify-content: flex-start; padding: 0px 12px 12px">
+                                          <span
+                                              style="
+                                                  font-size: 14px;
+                                                  font-family: 'GolosUI_Medium';
+                                                  color: #000;
+                                                  font-weight: 500;
+                                                  line-height: 18px;
+                                              "
+                                              >Date:
+                                          </span>
+                                          <span
+                                              style="
+                                                  margin-left: 8px;
+                                                  font-size: 14px;
+                                                  color: rgba(0, 0, 0, 0.75);
+                                                  font-family: 'GolosUIWebRegular';
+                                                  font-weight: 400;
+                                                  line-height: 18px;
+                                              "
+                                              >${params.name}</span
+                                          >
+                                      </div>
+                                      <div style="width: 100%; height: 8px; background-color: rgba(61, 80, 255, 0.1)"></div>
+                                  </div>
+                                  <div
+                                      style="
+                                          border-top: 8px solid #3d50ff;
+                                          border-right: 5px solid transparent;
+                                          border-bottom: 8px solid transparent;
+                                          border-left: 5px solid transparent;
+                                      "
+                                  ></div>
+                              </div>`;
+                }
+            },
+            yAxis: [
+                {
+                    type: 'value',
+                    axisLabel: {
+                        margin: 16,
+                        textStyle: {
+                            color: '#000',
+                            fontSize: 14,
+                            fontFamily: 'GolosUIWebRegular',
+                            fontWeight: 400,
+                            lineHeight: 18
+                        }
+                    },
+                    splitLine: {
+                        lineStyle: {
+                            color: 'rgba(0,0,0,0.1)'
+                        }
+                    }
+                }
+            ],
+            xAxis: [
+                {
+                    type: 'category',
+                    data: [
+                        '10-20',
+                        'Tue',
+                        'Wed',
+                        'Thu',
+                        'Fri',
+                        'Sat',
+                        '10-20',
+                        'Tue',
+                        'Wed',
+                        'Thu',
+                        'Fri',
+                        'Sat',
+                        '10-20',
+                        'Tue',
+                        'Wed',
+                        'Thu',
+                        'Fri',
+                        'Sat',
+                        '10-20',
+                        'Tue',
+                        'Wed',
+                        'Thu',
+                        'Fri',
+                        'Sat',
+                        '10-20',
+                        'Tue',
+                        'Wed',
+                        'Thu',
+                        'Fri',
+                        'Sat'
+                    ],
+                    axisTick: {
+                        show: false
+                    },
+                    axisLabel: {
+                        interval: 3,
+                        margin: 8,
+                        textStyle: {
+                            color: '#000',
+                            fontSize: 14,
+                            fontFamily: 'GolosUIWebRegular',
+                            fontWeight: 'normal',
+                            lineHeight: 18
+                        }
+                    },
+                    axisLine: {
+                        lineStyle: {
+                            color: 'rgba(0,0,0,0.15)',
+                            width: 2
+                        }
+                    }
+                }
+            ],
+            series: [
+                {
+                    type: 'bar',
+                    barWidth: 10,
+                    data: [
+                        10, 52, 100, 334, 390, 330, 10, 52, 100, 334, 390, 330, 10, 52, 100, 334,
+                        390, 330, 10, 52, 100, 334, 390, 330, 10, 52, 100, 334, 390, 330
+                    ],
+                    itemStyle: {
+                        color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
+                            { offset: 0, color: 'rgba(61,80,255,0.6)' },
+                            { offset: 1, color: 'rgba(61,80,255,0.35)' }
+                        ])
+                    },
+                    emphasis: {
+                        itemStyle: {
+                            color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
+                                { offset: 0, color: '#3D50FF' },
+                                { offset: 1, color: 'rgba(61,80,255,0.35)' }
+                            ])
+                        }
+                    }
+                    // todo dj 效果差，需沟通
+                    // markPoint: {
+                    //     symbol: 'circle',
+                    //     silent: true,
+                    //     symbolOffset: [0, '-50%'],
+                    //     label: {
+                    //         color: 'red'
+                    //     },
+                    //     // symbolSize: 2,
+                    //     data: [{ type: 'max', name: 'Max' }]
+                    // },
+                    // markLine: {
+                    //     silent: true,
+                    //     data: [
+                    //         {
+                    //             type: 'max'
+                    //         }
+                    //     ],
+                    //     label: {
+                    //         position: 'start',
+                    //         padding: 11.5,
+                    //         color: 'blue'
+                    //     },
+                    //     lineStyle: {
+                    //         type: [2, 3],
+                    //         color: 'rgba(61,80,255,0.35)'
+                    //     },
+                    //     symbol: 'none'
+                    // }
+                }
+            ]
+        };
+        relayedTrendChart.setOption(option, true);
+        window.addEventListener('resize', () => {
+            relayedTrendChart.resize();
+        });
+    });
 </script>
 
 <style lang="less" scoped>
@@ -99,6 +352,13 @@
             .flex(row, nowrap, space-between, flex-start);
             &__left {
                 flex: 1;
+                &__top {
+                    &__relayed_trend_wrap {
+                        :deep(.info_card__primary) {
+                            padding: 0;
+                        }
+                    }
+                }
                 &__bottom {
                     .flex(row, nowrap, space-between, flex-start);
                     &__transfer_type_wrap {
@@ -117,5 +377,6 @@
                 flex: 1;
             }
         }
+        flex-direction: column;
     }
 </style>
