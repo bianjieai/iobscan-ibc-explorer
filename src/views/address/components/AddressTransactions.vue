42ff37a65cbe979989a0572db9089b17c9d066f7
duanjie-cmd <18870420982@139.com>
2022-12-07 15:28:21 +0800

    add address structure

---
+++ src/views/address/components/AddressTransactions.vue
@@ -0,0 +1,8 @@
+<template>
+    <!-- todo shanshan -->
+    <InfoCard icon="icon-transactions" title="Transactions"></InfoCard>
+</template>
+
+<script setup></script>
+
+<style lang="less" scoped></style>
---
+++ src/views/address/components/BaseInfo.vue
@@ -0,0 +1,8 @@
+<template>
+    <!-- todo shanshan -->
+    <InfoCard icon="icon-a-baseinfo" title="Base Info"></InfoCard>
+</template>
+
+<script setup></script>
+
+<style lang="less" scoped></style>
