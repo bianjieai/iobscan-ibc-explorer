86656bc6be4bcb170979829359a18c9e22eed940
shanshan <shan@bianjie.ai>
2023-01-13 16:49:33 +0800

    optimize

--- src/views/overview/distribution/index.vue
+++ src/views/overview/distribution/index.vue
@@ -32,7 +32,10 @@
                     :type="distributionNoDataType"
                     class="distribution__failed"
                 />
-                <div v-else class="distribution__chart__wrap">
+                <div
+                    v-show="!distributionLoading && !distributionNoDataType"
+                    class="distribution__chart__wrap"
+                >
                     <div ref="distributionDom" class="distribution__chart__dom"></div>
                 </div>
             </div>
